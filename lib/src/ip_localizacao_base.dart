040e8f23161ea86761a1ff5360afd0567345891f
Mateus Penha <mateuspenha@gmail.com>
2019-02-13 16:37:34 -0200

    Implementação de consulta http e tratamento de retornos
    Implementação de consulta http e tratamento de retornos

--- lib/src/ip_localizacao_base.dart
+++ lib/src/ip_localizacao_base.dart
@@ -1,39 +1,94 @@
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class ip_externo {
-  String _URLBase = 'http://checkip.dyndns.org/';
+  String _URLBase = 'https://ipinfo.io/%/json';
 
-  String _IP;
+  String _LastIP;
+
+  // variables return
+  String _return01, _return02, _return03, _return04, _return05, _return06, _return07, _return08;
 
   int _Response;
 
   String _Body;
 
-  ip_externo() {
-    _IP = '';
+  ip_localizacao() {
+    clear();
+
+    _LastIP = null;
+
+    _Body = null;
+
+    _Response = null;
   }
 
-  Future ReadIP() async {
-    var response = await http.get(_URLBase);
+  void clear() {
+    _return01 = null;
+    _return02 = null;
+    _return03 = null;
+    _return04 = null;
+    _return05 = null;
+    _return06 = null;
+    _return07 = null;
+    _return08 = null;
+  }
+
+  Future ReadIP(String IP) async {
+    _LastIP = IP;
+
+    var response = await http.get(_URLBase.replaceAll('%',IP));
 
     _Response = response.statusCode;
     _Body = response.body;
 
     if (_Response != 200)
-      _IP = '';
+      clear();
     else {
-      int position_01 = _Body.indexOf('<body>')+6;
+      Map<String, dynamic> IPDate = jsonDecode(_Body);
 
-      int position_02 = _Body.indexOf('</body>');
+      _return01 = IPDate['ip'];
+      _return02 = IPDate['hostname'];
+      _return03 = IPDate['city'];
+      _return04 = IPDate['region'];
+      _return05 = IPDate['country'];
+      _return06 = IPDate['loc'];
+      _return07 = IPDate['postal'];
+      _return08 = IPDate['org'];
 
-      _IP = _Body.substring(position_01,position_02).trim();
-
-      _IP = _IP.replaceAll('Current IP Address:', '').trim();
     }
   }
 
   String getIP() {
-    return _IP;
+    return _return01;
+  }
+
+  String getHostname() {
+    return _return02;
+  }
+
+  String getCity() {
+    return _return03;
+  }
+
+  String getRegion() {
+    return _return04;
+  }
+
+  String getCountry() {
+    return _return05;
+  }
+
+  String getCoordinates() {
+    return _return06;
+  }
+
+  String getPostal() {
+    return _return07;
+  }
+
+  String getOrg() {
+    return _return08;
   }
 
   int getResponse() {
